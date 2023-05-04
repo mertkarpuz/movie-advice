7df7751e8a07f40a1a80f041e12291f9829703a7
MertKarpuz <mert.karpuz@outlook.com>
2023-05-04 16:27:57 +0300

    generic restsharp get function

--- src/Application/MovieAdvice.Application/Utilities/HttpUtilities.cs
+++ src/Application/MovieAdvice.Application/Utilities/HttpUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,20 @@ namespace MovieAdvice.Application.Utilities
 {
     public class HttpUtilities
     {
+        public string? ExecuteGetHttpRequest(string endpoint, Dictionary<string,string>? headers)
+        {
+            RestClient client = new(endpoint);
+            RestRequest request = new(endpoint, Method.Get);
+            if (headers != null)
+            {
+                foreach (KeyValuePair<string, string> header in headers)
+                {
+                    request.AddHeader(header.Key, header.Value);
+                }
+            }
+            RestResponse response = client.ExecuteAsync(request).GetAwaiter().GetResult();
+
+            return response.Content;
+        }
     }
 }
